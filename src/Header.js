62f74ef73fa078855f57830156da327c40581131
tabufx50 <44902903+tabufx50@users.noreply.github.com>
2021-03-29 14:04:55 -0500

    first commmit

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+
+function Header() {
+    return (
+        <div className='header'>
+            
+
+            <div className="header__left">
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
+                <div className="header__search">
+                    
+                    <SearchIcon />
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption title="My Network" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
